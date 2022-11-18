ca6a80099b31dbf3d29cb25c5c93a6fb2bad0200
mateoroca <mateojoserocaclemntis@gmail.com>
2022-11-18 15:05:43 -0300

    delete some files

--- Drone/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/mateo/OneDrive/Documentos/mateo/ANALISTA DE SISTEMAS/SISTEMAS-2do/AYEDII/MESA_DE_TRABAJO/Drone
-# Build directory: C:/Users/mateo/OneDrive/Documentos/mateo/ANALISTA DE SISTEMAS/SISTEMAS-2do/AYEDII/MESA_DE_TRABAJO/Drone/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(UnitTests "runUnitTests")
-set_tests_properties(UnitTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/mateo/OneDrive/Documentos/mateo/ANALISTA DE SISTEMAS/SISTEMAS-2do/AYEDII/MESA_DE_TRABAJO/Drone/CMakeLists.txt;34;add_test;C:/Users/mateo/OneDrive/Documentos/mateo/ANALISTA DE SISTEMAS/SISTEMAS-2do/AYEDII/MESA_DE_TRABAJO/Drone/CMakeLists.txt;0;")
-subdirs("googletest")
--- Drone/build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/mateo/OneDrive/Documentos/mateo/ANALISTA DE SISTEMAS/SISTEMAS-2do/AYEDII/MESA_DE_TRABAJO/Drone/googletest
-# Build directory: C:/Users/mateo/OneDrive/Documentos/mateo/ANALISTA DE SISTEMAS/SISTEMAS-2do/AYEDII/MESA_DE_TRABAJO/Drone/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
